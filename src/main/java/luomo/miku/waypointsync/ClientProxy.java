239aa39e6bef93ec3b01b698d46640799980305c
luomolhx <2932965369@qq.com>
2025-10-08 00:15:39 +0800

    init

---
+++ src/main/java/luomo/miku/waypointsync/ClientProxy.java
@@ -0,0 +1,9 @@
+package luomo.miku.waypointsync;
+
+
+public class ClientProxy extends CommonProxy {
+
+    // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
+    // Don't forget to call the super methods as well.
+
+}

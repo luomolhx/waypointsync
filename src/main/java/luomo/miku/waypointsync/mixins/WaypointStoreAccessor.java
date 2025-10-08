cf69f0f5f216df224516d9d8f993b4d4461f4ec4
luomolhx <2932965369@qq.com>
2025-10-08 19:11:56 +0800

    spotless

--- src/main/java/luomo/miku/waypointsync/mixins/WaypointStoreAccessor.java
+++ src/main/java/luomo/miku/waypointsync/mixins/WaypointStoreAccessor.java
@@ -1,15 +1,15 @@
 package luomo.miku.waypointsync.mixins;
 
-import journeymap.client.model.Waypoint;
-import journeymap.client.waypoint.WaypointStore;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
+
+import journeymap.client.model.Waypoint;
+import journeymap.client.waypoint.WaypointStore;
 
 @Mixin(WaypointStore.class)
 public interface WaypointStoreAccessor {
 
     @Invoker("writeToFile")
     boolean invokeWriteToFile(Waypoint waypoint);
-
 
 }
